138a625705b2027d56b43543992b3fc309dd5ecb
Vishawanath kokare <vishkokre@gmail.com>
2020-05-26 14:05:57 +0530

    Update install-jupyter.sh
    making it's workfull and runful for run jupyter notebook on termux

--- install-jupyter.sh
+++ install-jupyter.sh
@@ -11,42 +11,26 @@ echo "#     # # ######## #      #        #       #        #"
 echo "#    #  #       ## #      #        #       #        #"
 echo " #  #   # ######## #      #        #       #        #"
 echo "  ##    # ######## #      # ######## ####### ########"
-
 sleep 3
 
-echo "$############THIS WAS MADE BY VISHAWANATH###############"
-echo "$########https://www.github.com/VISHAWANATH953##########"
-
-echo "$###############DO NOT COPY THIS########################"
-echo "$#########ONLY PULL REQUEST AVALIBALE###################"
-
+echo "############THIS WAS MADE BY VISHAWANATH#############"
+echo "########https://www.github.com/VISHAWANATH953########"
+echo "#####SPEACIAL THANKS TO WMCB TECH TO INSPIRE ME######"
 sleep 5
 
 #Making some update && upgrade && installs && files to rady it for.
-apt update && apt upgrade -y && apt install python -y && pip3 install --upgrade pip && pip3 install virtualenv && virtualenv env && clear
+apt update && apt upgrade -y && apt install python clang libzmq libcrypt -y && pip3 install --upgrade pip && pip3 install jupyter && clear
 
-bin=start-jupyter.sh
+bin=start-ju.sh
 echo "##############WRITING LANCH SCRIPT#####################"
 sleep 5
 cat > $bin <<- EOM
-#!/bin/bash
-. env/bin/acitvate
+#!/data/data/com.termux/files/usr/bin/bash
+
+jupyter notebook
 EOM
-cp start-jupyter.sh /data/data/com.termux/files/home/start-jupyter.sh
-chmod +x /data/data/com.termux/files/home/start-jupyter.sh
+chmod +x /data/data/com.termux/files/home/start-ju.sh
 
 #Making some file so launch it.
 echo "$############START TO JUPYTER NOTEBOOK RUN ./start-jupyter.sh####"
-echo "#################BUT IT IS AUTOMATIC START#######################"
-echo "$##################WHEN run jupyter notebook#####################"
-echo "$###ALSO YOU CAN RUN MANY PYTHON FRAMEWORK IN THIS ENVIROMENT####"
 sleep 5
-
-
-cd /data/data/com.termux/files/home/
-
-rm -rf jupyternotebook_termux
-
-. env/bin/activate
-
-
